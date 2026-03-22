cbac319c9b2e1753bde73d75527fe7058be5bf52
dityakp <useradityakp@gmail.com>
2026-03-22 23:03:00 +0530

    Updated AWS region to Mumbai and updated gitignore

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -1,5 +1,20 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "scamshield_server" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   
   vpc_security_group_ids = [aws_security_group.scamshield_sg.id]
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type (Free Tier eligible)"
   type        = string
   default     = "t2.micro"
-}
-
-variable "ami_id" {
-  description = "Ubuntu 22.04 LTS AMI ID for us-east-1"
-  type        = string
-  default     = "ami-0c7217cdde317cfec"
 }
